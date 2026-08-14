108f7da3aaee285d2db8fdb669944cfffd5c1495
Caleb <caleb.prestidge@kingsway.school.nz>
2026-08-14 12:02:55 +1200

    Started with the Boss

---
+++ 1The-Legend-of-Rata-main/the-legend-of-rata/boss_state.gd
@@ -0,0 +1,17 @@
+class_name BossState
+extends Node
+
+var boss: CharacterBody2D
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func update(_delta: float) -> void:
+	pass
+
+func physics_update(_delta: float) -> void:
+	pass
+	
