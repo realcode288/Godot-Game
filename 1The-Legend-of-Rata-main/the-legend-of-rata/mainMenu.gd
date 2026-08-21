abc4ee7da28a7e3292550a7cf136a91718e424f5
Caleb <caleb.prestidge@kingsway.school.nz>
2026-08-21 12:01:48 +1200

    Fixed Main Menu

--- 1The-Legend-of-Rata-main/the-legend-of-rata/mainMenu.gd
+++ 1The-Legend-of-Rata-main/the-legend-of-rata/mainMenu.gd
@@ -1,8 +1,11 @@
 extends Control
 
-
-
-
-
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/lore.tscn")
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/controls.tscn")
