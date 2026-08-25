3f6b4e2ed56f02849a6f54819f4d595f89c661ed
Caleb <caleb.prestidge@kingsway.school.nz>
2026-08-25 14:53:25 +1200

    Started making a speedrun mode

--- 1The-Legend-of-Rata-main/the-legend-of-rata/mainMenu.gd
+++ 1The-Legend-of-Rata-main/the-legend-of-rata/mainMenu.gd
@@ -9,3 +9,6 @@ func _on_button_3_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/controls.tscn")
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
